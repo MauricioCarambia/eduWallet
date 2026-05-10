31bd22b5da5b97b04817a2384cc3ce5bb4e440c3
Mauricio Carambia <mcarambia@gmail.com>
2026-05-09 22:30:55 -0300

    fix rutas pagos duplicadas

--- backend/src/routes/pagos.js
+++ backend/src/routes/pagos.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { crearPreferencia, procesarPago, webhook } = require('../controllers/pagosController');
-const { verificarPadre } = require('../middlewares/auth');
 const { crearPreferencia, procesarPago, webhook, verificarPago } = require('../controllers/pagosController');
+const { verificarPadre } = require('../middlewares/auth');
 
 router.post('/preferencia', verificarPadre, crearPreferencia);
 router.post('/procesar', verificarPadre, procesarPago);

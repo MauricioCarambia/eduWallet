cf935b77e079e3b1105d5fe10aa5b6bcde8c224e
Mauricio Carambia <mcarambia@gmail.com>
2026-05-11 23:46:31 -0300

    anular venta en POS

--- backend/src/routes/transacciones.js
+++ backend/src/routes/transacciones.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { getTransacciones, getTransaccionesAlumno, cobrar } = require('../controllers/transaccionesController');
 const { verificarToken, soloAdmin } = require('../middlewares/auth');
 const { getTransacciones, getTransaccionesAlumno, cobrar, anularVenta } = require('../controllers/transaccionesController');
 
